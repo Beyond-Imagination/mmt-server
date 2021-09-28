f7ad2b4a4a4ee07f1a240d3c09dd0fb97b5a1762
구원 라 <laggu91@gmail.com>
2021-09-28 14:49:01 +0000

    add authenticate middleware
    Merge-request: TOURISM-KAKAO-MR-13

--- src/plugins/passport.plugin.ts
+++ src/plugins/passport.plugin.ts
@@ -1,10 +1,12 @@
 import passport from 'passport'
 import LocalStrategy from '@/strategies/passport/local.passport'
 import JwtStrategy from '@/strategies/passport/jwt.passport'
+import TokenStrategy from '@/strategies/passport/token'
 import { Model } from '@/types/model.type'
 
 passport.use(LocalStrategy)
 passport.use(JwtStrategy)
+passport.use(TokenStrategy)
 
 passport.serializeUser((user, done) => done(null, user))
 passport.deserializeUser<Model.User>((user, done) => done(null, user))
---
+++ src/strategies/passport/token.ts
@@ -0,0 +1,26 @@
+import { Strategy } from 'passport-accesstoken'
+
+import Account from '@/models/account'
+
+let strategyOptions = {
+    tokenHeader:    'Authorization',    
+};
+
+export default new Strategy(strategyOptions,
+    function (token, done) {
+        if(!token.startsWith("Bearer ")){
+            return done(null, false);
+        }
+        token = token.split(" ")[1];
+        Account.findOne({accessToken: token}, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+
+            if (!user) {
+                return done(null, false);
+            }
+
+            return done(null, user);
+        });
+    });
