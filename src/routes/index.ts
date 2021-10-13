6ce9f5af5860859f224d58ddc94ffb227041e967
구원 라 <laggu91@gmail.com>
2021-10-13 15:15:46 +0000

    TOURISM-KAKAO-MR-37 dev create user api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,14 @@ import auth from '@/routes/modules/auth'
 import tour from '@/routes/modules/tour'
 import nft from '@/routes/modules/nft'
 import users from '@/routes/modules/user'
+import dev from '@/routes/modules/dev'
 
 import { Server } from '@/types/server.type'
 
 const routes: Server.IRoute[] = [articles, auth, tour, nft, users]
+
+if(process.env.NODE_ENV === 'development') {
+    routes.push(dev);
+}
 
 export default routes
---
+++ src/routes/modules/dev.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { body, validationResult } from 'express-validator';
+import { v4 as uuid } from 'uuid';
+
+import { wrapAsync } from '@/middlewares';
+import { success } from '@/helpers'
+import User from '@/models/user'
+
+const router = Router();
+
+router.post('/user',
+    wrapAsync(async(req, res, next) => {
+        let user = new User({
+            "name": req.body.name || "홍길동",
+            "profileImageUri": req.body.profile_image || "https://beyond-imagination-moment.s3.ap-northeast-2.amazonaws.com/dev/%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%A5%E1%86%AB.png",
+            "klaytnAddress": req.body.klaytn_address || "0x308061b1593a835E3298F067A40c8e1b2cB05D9D",
+            "kakaoUserId": req.body.kakao_user_id || Math.floor(Math.random()*1000000),
+            "accessToken": req.body.access_token || uuid(),
+        })
+        await user.save();
+        success(res, {"access_token": user.accessToken});
+    })
+)
+
+export default {
+    name: 'dev',
+    router
+}
