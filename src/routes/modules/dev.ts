69c31465b11c3364ea21fb810ed9d791cf3ff6cb
구원 라 <laggu91@gmail.com>
2021-10-15 10:01:42 +0000

    TOURISM-KAKAO-MR-42 fix field name

--- src/routes/modules/dev.ts
+++ src/routes/modules/dev.ts
@@ -11,7 +11,7 @@ const router = Router();
 router.post('/user',
     wrapAsync(async(req, res, next) => {
         let user = new User({
-            "name": req.body.name || "홍길동",
+            "nickname": req.body.nickname || "홍길동",
             "profileImageUri": req.body.profile_image || "https://beyond-imagination-moment.s3.ap-northeast-2.amazonaws.com/dev/%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%A5%E1%86%AB.png",
             "klaytnAddress": req.body.klaytn_address || "0x308061b1593a835E3298F067A40c8e1b2cB05D9D",
             "kakaoUserId": req.body.kakao_user_id || Math.floor(Math.random()*1000000),
