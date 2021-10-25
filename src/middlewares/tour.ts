e5a749b93437eedf8dd9a82ac693e4af7c35baf3
SimDaSong <song98987@korea.ac.kr>
2021-10-25 16:38:24 +0000

    TOURISM-KAKAO-MR-56 fix tour validator error
    Co-authored-by: SimDaSong <song98987@korea.ac.kr>

--- src/middlewares/tour.ts
+++ src/middlewares/tour.ts
@@ -1,18 +1,18 @@
-import {param, query} from "express-validator";
-import {ARRANGE_VALUES, CONTENT_TYPE_ID_VALUES} from "@/constants";
-import {isExistAndIn, isExistAndNumeric, isIn} from "@/helpers";
+import {param, query} from 'express-validator'
+import {ARRANGE_VALUES, CONTENT_TYPE_ID_VALUES} from '@/constants'
+import {isExistAndIn, isExistAndNumeric, isIn} from '@/helpers'
 
 export const validateGetManyRequest = [
-    query('numOfRows', '정수여야 합니다.').optional().isInt(),
-    query('pageNo', '정수여야 합니다.').optional().isInt(),
-    query('arrange').optional().custom(value => isIn(value, ARRANGE_VALUES)),
-    query('contentTypeId').optional().custom(value => isIn(value, CONTENT_TYPE_ID_VALUES)),
-    query('mapX').custom(isExistAndNumeric),
-    query('mapY').custom(isExistAndNumeric),
-    query('radius').custom(isExistAndNumeric),
+  query('numOfRows', '정수여야 합니다.').optional().isInt(),
+  query('pageNo', '정수여야 합니다.').optional().isInt(),
+  query('arrange').optional().custom(value => isIn(value, ARRANGE_VALUES)),
+  query('contentTypeId').optional().custom(value => isIn(value, CONTENT_TYPE_ID_VALUES)),
+  query('mapX').custom(isExistAndNumeric),
+  query('mapY').custom(isExistAndNumeric),
+  query('radius').custom(isExistAndNumeric),
 ]
 
 export const validateGetOneRequest = [
-    param('id').exists(),
-    query('contentTypeId').custom(isExistAndIn),
+  param('id').exists(),
+  query('contentTypeId').custom(value => isExistAndIn(value, CONTENT_TYPE_ID_VALUES)),
 ]
